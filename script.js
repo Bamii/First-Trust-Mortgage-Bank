8bc69e75b250385fb28436e44e108761cba5c1e0
grace22411 <olayinkagrace112.ob@gmail.com>
2019-10-27 20:10:59 -0700

    updated

--- script.js
+++ script.js
@@ -21,15 +21,81 @@
     
     //Header Dropdown
     $("#acct").click(function(){
-        $(".account").slideToggle();
+        $(".savings").slideUp();
+        $(".media").slideUp();
+        $(".investor").slideUp();
+        $(".about-us").slideUp();
+        $(".Loan").slideUp();
+        $(".account").delay(300).slideToggle();
         $("header").css({"background-color": "#f7f7f7", "color" : "#54575C"});
+        $(".hide").css({"display": "inline-block"});
+        $(".show").css({"display": "none"});
     });
      
-      $(".next_one").click(function(){
+     
+     //Next dropdown
+    $("#loan").click(function(){
         $(".account").slideUp();
-        $(".Loan").slideToggle();
+        $(".savings").slideUp();
+        $(".media").slideUp();
+        $(".investor").slideUp();
+        $(".about-us").slideUp();
+        $(".Loan").delay(500).slideToggle();
         $("header").css({"background-color": "#f7f7f7", "color" : "#54575C"});
+        $(".hide").css({"display": "inline-block"});
+        $(".show").css({"display": "none"});
     });
+     
+     
+     //Next dropdown
+       $("#saving").click(function(){
+        $(".account").slideUp();
+        $(".Loan").slideUp();
+        $(".media").slideUp();
+        $(".investor").slideUp();
+        $(".about-us").slideUp();
+        $(".savings").delay(500).slideToggle();
+        $("header").css({"background-color": "#f7f7f7", "color" : "#54575C"});
+        $(".hide").css({"display": "inline-block"});
+        $(".show").css({"display": "none"});
+    });
+     
+    $("#about").click(function(){
+        $(".account").slideUp();
+        $(".Loan").slideUp();
+        $(".savings").slideUp();
+        $(".media").slideUp();
+        $(".investor").slideUp();
+        $(".about-us").delay(500).slideToggle()
+        $("header").css({"background-color": "#f7f7f7", "color" : "#54575C"});
+        $(".hide").css({"display": "inline-block"});
+        $(".show").css({"display": "none"});
+    });
+     
+     $("#media").click(function(){
+        $(".account").slideUp();
+        $(".Loan").slideUp();
+        $(".savings").slideUp();
+        $(".media").delay(500).slideToggle();
+        $(".investor").slideUp();
+        $(".about-us").slideUp();
+        $("header").css({"background-color": "#f7f7f7", "color" : "#54575C"});
+        $(".hide").css({"display": "inline-block"});
+         $(".show").css({"display": "none"});
+    });
+     
+    $("#investor").click(function(){
+        $(".account").slideUp();
+        $(".Loan").slideUp();
+        $(".savings").slideUp();
+        $(".media").slideUp();
+        $(".investor").delay(500).slideToggle();
+        $(".about-us").slideUp();
+        $("header").css({"background-color": "#f7f7f7", "color" : "#54575C"});
+        $(".hide").css({"display": "inline-block"});
+        $(".show").css({"display": "none"});
+    }); 
+     
      
     
     $(".dropdown").hover(            
@@ -43,6 +109,21 @@
         }
     );
 });
+
+//Banner background image slide show
+ var folder = "";
+ var pictures=["images/Family-II.png", "images/5d759496aed63.png", "images/realty%20Finance.jpg", "images/Savings.jpg"];
+ var num = 0;
+          
+    function main(){
+       var slider = document.getElementById("section");
+           if (num == 3) num = 0;
+		   else num++;
+           
+            slider.style.backgroundImage = "linear-gradient(rgba(0,0,0,1),rgba(0,0,0,0.1)),url("+pictures[num]+")"  ;
+        }
+            
+        setInterval('main()', 3000);
 
 
 function openCity(evt, cityName) {
